ed229e5b91403eaf67a1eea6845546e473cb49f9
Graham Schilling <gschillling@Grahams-MacBook-Pro.local>
2015-12-27 18:06:13 -0500

    Initial Commit

---
+++ CCStats/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CCStats
+//
+//  Created by Graham Schilling on 12/27/15.
+//  Copyright (c) 2015 Graham Schilling. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
